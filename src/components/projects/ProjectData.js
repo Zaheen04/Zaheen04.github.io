fa2bd61600a937b21539040776991c9cb5aab5ba
Zaheen04 <khanzaheen799@gmail.com>
2024-01-19 02:53:38 +0530

    images

--- src/components/projects/ProjectData.js
+++ src/components/projects/ProjectData.js
@@ -6,17 +6,17 @@ export const projectData = [{
     link : "https://e-commercebenevolent-boot.vercel.app/",
     description : "Shopcart is a full-fledged e-commerce website designed to provide users with a seamless and intuitive online shopping experience. The website encompasses a wide range of products across various categories, ensuring that users can find and purchase items of their choice. ",
     techList : ["React", "Javascript", "chakra-ui-react","Redux"],
-    screenshot : profile
+    screenshot : homepage_ecommerce
 }, {
     name : "SkinStore",
     link : "https://high-fruit.netlify.app/",
     description : "Welcome to the SkinStore Clone project! This project is a clone of the SkinStore website, created by a team of 5 members using HTML, CSS, and JavaScript.",
     techList : ["Css", "Javascript", "HTML"],
-    screenshot : resume
+    screenshot : skinstore
 }, {
     name : "Glossier",
     link : "https://remarkable-khapse-1c1e63.netlify.app/",
     description : "Glossier is a new approach to beauty. its about fun and freedom and being OK with yourself today. We make intuitive, uncomplicated products designed to live with you.",
     techList : ["Css", "Javascript", "HTML"],
-    screenshot : profile
+    screenshot : Glossier
 }]
